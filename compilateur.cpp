a84312ed672a81ccff30b9b17b797dd2119d3661
Lenovo <willbox500@gmail.com>
2021-03-29 13:01:53 +0200

    Projet initial

--- compilateur.cpp
+++ compilateur.cpp
@@ -24,34 +24,74 @@
 
 using namespace std;
 
-char current;				// Current car	
+char current, lookedAhead;                // Current char    
+int NLookedAhead=0;
 
-void ReadChar(void){		// Read character and skip spaces until 
-				// non space character is read
-	while(cin.get(current) && (current==' '||current=='\t'||current=='\n'))
-	   	cin.get(current);
+void ReadChar(void){
+    if(NLookedAhead>0){
+        current=lookedAhead;    // Char has already been read
+        NLookedAhead--;
+    }
+    else
+        // Read character and skip spaces until 
+        // non space character is read
+        while(cin.get(current) && (current==' '||current=='\t'||current=='\n'));
+}
+
+void LookAhead(void){
+    while(cin.get(lookedAhead) && (lookedAhead==' '||lookedAhead=='\t'||lookedAhead=='\n'));
+    NLookedAhead++;
 }
 
 void Error(string s){
 	cerr<< s << endl;
 	exit(-1);
 }
-
+// DeclarationPart := "[" Letter {"," Letter} "]"
 // ArithmeticExpression := Term {AdditiveOperator Term}
 // Term := Digit | "(" ArithmeticExpression ")"
 // AdditiveOperator := "+" | "-"
 // Digit := "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"
-
+void Vergule(void);
+void Letter(void);
+void DeclarationPart(void){
+   if (current=='['){
+	   cout << "\t       .data"<<endl;
+	   ReadChar();
+	   Letter();
+	   
+	   Vergule();
+       if(current!=']')
+			Error("']' était attendu");		// ")" expected
+		else
+			ReadChar();
+   }
+}
+void Vergule(void){
+	while (current == ','){
+       ReadChar();
+	   Letter();
+	}
+}
+void Letter(void){
 	
+	if(current =='a'||current =='b'||current =='c'){
+		cout <<"\t"<<current<<"      .quad 0"<<endl;
+		ReadChar();
+	}else{
+		Error("lettre attendu");		   // Digit expected
+	}
+}
+
 void AdditiveOperator(void){
-	if(current=='+'||current=='-')
+	if(current=='+'||current=='-'||current=='>')
 		ReadChar();
 	else
 		Error("Opérateur additif attendu");	   // Additive operator expected
 }
 		
 void Digit(void){
-	if((current<'0')||(current>'9'))
+	if((current<'0'))
 		Error("Chiffre attendu");		   // Digit expected
 	else{
 		cout << "\tpush $"<<current<<endl;
@@ -71,7 +111,7 @@ void Term(void){
 			ReadChar();
 	}
 	else 
-		if (current>='0' && current <='9')
+		if (current>='0')
 			Digit();
 	     	else
 			Error("'(' ou chiffre attendu");
@@ -80,7 +120,7 @@ void Term(void){
 void ArithmeticExpression(void){
 	char adop;
 	Term();
-	while(current=='+'||current=='-'){
+	while(current=='+'||current=='-'||current=='>'){
 		adop=current;		// Save operator in local variable
 		AdditiveOperator();
 		Term();
@@ -88,8 +128,13 @@ void ArithmeticExpression(void){
 		cout << "\tpop %rax"<<endl;	// get second operand
 		if(adop=='+')
 			cout << "\taddq	%rbx, %rax"<<endl;	// add both operands
-		else
-			cout << "\tsubq	%rbx, %rax"<<endl;	// substract both operands
+		else{
+			if(adop=='-'){
+			    cout << "\tsubq	%rbx, %rax"<<endl;	// substract both operands
+			}else{
+				cout << "\tcmp %rbx, %rax"<<endl;	// substract both operands
+			}
+		}
 		cout << "\tpush %rax"<<endl;			// store result
 	}
 
@@ -105,6 +150,7 @@ int main(void){	// First version : Source code on standard input and assembly co
 
 	// Let's proceed to the analysis and code production
 	ReadChar();
+	DeclarationPart();
 	ArithmeticExpression();
 	ReadChar();
 	// Trailer for the gcc assembler / linker
